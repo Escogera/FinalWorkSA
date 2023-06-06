466295a4c7fc1e83aa4cef6301872d452c83fca2
Vladimir <maostyle@mail.ru>
2023-06-06 20:43:55 +0300

    complete work

---
+++ Program/Model/Dog.java
@@ -0,0 +1,20 @@
+package Program.Model;
+
+import java.util.ArrayList;
+
+
+/* Дочерний класс */
+public class Dog extends Pet {
+
+    public Dog(){
+
+    }
+
+    public Dog(int petID, String petName, String petType, ArrayList<String> commands) {
+        setPetID(petID);
+        setPetName(petName);
+        setPetType(petType);
+        setCommands(commands);
+    }
+
+}
